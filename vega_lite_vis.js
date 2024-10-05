ed2147a9381da3e900b9d41b619181e7a614608c
JoelTheDecent04 <jkru0007@student.monash.edu>
2024-10-06 05:10:36 +1100

    Week 10 Homework

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,9 @@
 var vg_1 = "simple_chart.vg.json";
+var vg_2 = "prop_symbol_chart.vg.json";
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#prop_symbol_chart", vg_2).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
