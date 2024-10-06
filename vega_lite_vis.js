23899aa8f5c47f92d92374b940f5297edcb7a94f
JoelTheDecent04 <jkru0007@student.monash.edu>
2024-10-06 12:14:53 +1100

    Changed layout

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,10 +1,5 @@
-var vg_1 = "simple_chart.vg.json";
 var vg_2 = "prop_symbol_chart.vg.json";
 var vg_3 = "bar_chart.vg.json";
-
-vegaEmbed("#simple_chart", vg_1).then(function(result) {
-// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
 
 vegaEmbed("#prop_symbol_chart", vg_2).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
