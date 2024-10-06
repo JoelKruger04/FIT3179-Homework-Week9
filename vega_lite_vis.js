06e2146f259dae330ca68b83ffbd1f7863445f9b
JoelTheDecent04 <jkru0007@student.monash.edu>
2024-10-06 11:04:27 +1100

    Added labels

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,9 +1,15 @@
 var vg_1 = "simple_chart.vg.json";
 var vg_2 = "prop_symbol_chart.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+var vg_3 = "bar_chart.vg.json";
+
+vegaEmbed("#simple_chart", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 vegaEmbed("#prop_symbol_chart", vg_2).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+vegaEmbed("#bar_chart", vg_3).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
